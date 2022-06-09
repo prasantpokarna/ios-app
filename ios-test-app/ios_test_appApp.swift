ee2044ded38debaec0e30afb7c29456b4039e623
Aakash Shirodkar <aakashshirodkar619@gmail.com>
2022-06-09 10:17:33 +0530

    test code changes

---
+++ ios-test-app/ios_test_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_test_appApp.swift
+//  ios-test-app
+//
+//  Created by Aakash Shirodkar on 08/06/22.
+//
+
+import SwiftUI
+
+@main
+struct ios_test_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
